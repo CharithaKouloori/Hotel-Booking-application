e5a73bc339f5fbebc908725fc5e3d312d91132a6
Charithakrishna Kouloori <kcharitha1216@gmail.com>
2025-11-11 18:02:25 +0530

    Add Flutter web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1183957500"
+    serviceWorkerVersion: "3010231586"
   }
 });
